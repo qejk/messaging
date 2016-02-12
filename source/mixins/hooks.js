f5d7e0bfb26689697552abe33cd314f0cfa4192e
qejk <qejk@qejk.net>
2016-02-12 03:31:08 +0100

    Adds initial waterfall method

--- source/mixins/hooks.js
+++ source/mixins/hooks.js
@@ -1,7 +1,119 @@
+var _setImmediate = typeof setImmediate === 'function' && setImmediate;
+
+var _delay = _setImmediate ? function(fn) {
+    // not a direct alias for IE10 compatibility
+    _setImmediate(fn);
+} : function(fn) {
+    setTimeout(fn, 0);
+};
+
+if (typeof process === 'object' && typeof process.nextTick === 'function') {
+    nextTick = process.nextTick;
+} else {
+    nextTick = _delay;
+}
+setImmediate = _setImmediate ? _delay : nextTick;
+
+
+
+function ensureAsync(fn) {
+  return _restParam(function (args) {
+    var callback = args.pop();
+    args.push(function () {
+      var innerArgs = arguments;
+      if (sync) {
+        setImmediate(function () {
+          callback.apply(null, innerArgs);
+        });
+      } else {
+        callback.apply(null, innerArgs);
+      }
+    });
+    var sync = true;
+    fn.apply(this, args);
+    sync = false;
+  });
+}
+
+function _once(fn) {
+  return function() {
+    if (fn === null) return;
+    fn.apply(this, arguments);
+    fn = null;
+  };
+}
+
+// Similar to ES6's rest param (http://ariya.ofilabs.com/2013/03/es6-and-rest-parameter.html)
+// This accumulates the arguments passed into an array, after a given index.
+// From underscore.js (https://github.com/jashkenas/underscore/pull/2140).
+function _restParam(func, startIndex) {
+    startIndex = startIndex == null ? func.length - 1 : +startIndex;
+    return function() {
+        var length = Math.max(arguments.length - startIndex, 0);
+        var rest = Array(length);
+        for (var index = 0; index < length; index++) {
+            rest[index] = arguments[index + startIndex];
+        }
+        switch (startIndex) {
+            case 0: return func.call(this, rest);
+            case 1: return func.call(this, arguments[0], rest);
+        }
+        // Currently unused but handle cases outside of the switch statement:
+        // var args = Array(startIndex + 1);
+        // for (index = 0; index < startIndex; index++) {
+        //     args[index] = arguments[index];
+        // }
+        // args[startIndex] = rest;
+        // return func.apply(this, args);
+    };
+}
+
+iterator = function (tasks) {
+  function makeCallback(index) {
+    function fn() {
+      if (tasks.length) {
+        tasks[index].apply(null, arguments);
+      }
+      return fn.next();
+    }
+    fn.next = function () {
+      return (index < tasks.length - 1) ? makeCallback(index + 1): null;
+    };
+    return fn;
+  }
+  return makeCallback(0);
+};
+
+
 Space.messaging.Hooks = {
 
   dependencies: {
-    _hooks: 'Space.messaging.HookRegistry'
+    _hooks: 'Space.messaging.HookRegistry',
+    underscore: 'underscore'
+  },
+
+  waterfall(tasks, callback) {
+    callback = _once(callback || noop);
+    if (!this.underscore.isArray(tasks)) {
+      var err = new Error('First argument to waterfall must be an array of functions');
+      return callback(err);
+    }
+    if (!tasks.length) {
+      return callback();
+    }
+    function wrapIterator(iterator) {
+      return _restParam(function (args) {
+        var next = iterator.next();
+        if (next) {
+          args.push(wrapIterator(next));
+        }
+        else {
+          args.push(callback);
+        }
+        ensureAsync(iterator).apply(null, args);
+      });
+    }
+    wrapIterator(iterator(tasks))();
   },
 
   addBeforeHook(messageType, hookClass, hook) {
@@ -43,5 +155,6 @@ Space.messaging.Hooks = {
 
   getRuleHooks(messageType) {
     return this.getHooks(messageType, 'rule');
-  }
+  },
+
 }
